90a9799a0bf997b378d43494cb658c10fabe7595
samisaeed <mahmud.saeed@selisegroup.com>
2024-02-11 23:58:08 +0600

    pull form main

--- src/app/main/home/home/home.component.ts
+++ src/app/main/home/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import html2canvas from "html2canvas";
 
 @Component({
     selector: 'app-home',
@@ -9,16 +10,21 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class HomeComponent implements OnInit {
     createForm: FormGroup;
     public isPersonActive = false;
-    public division = [{ name: 'cumilla', city: 'CUMILLA' }];
-    public person = [{ name: 'tanim', person: 'Tamin' }];
+    public division = [{ name: 'কুমিল্লা', city: 'কুমিল্লা' }];
+    public person = [{ name: 'কুমিল্লা', person: 'কুমিল্লা - ৬' }];
 
     assetBannerFileNames = [
         'tem-1', 'tem-2', 'tem-3'
-    ]
+    ];
+    @ViewChild('screen', {static: false}) screen: ElementRef;
+    @ViewChild('canvas', {static: false}) canvas: ElementRef;
+    @ViewChild('downloadLink', {static: false}) downloadLink: ElementRef;
 
     avatarSrc: any;
     name: any = '';
     designation: any = '';
+    isActiveTemplate = 0 ;
+    capturedImage;
 
     constructor(private formBuilder: FormBuilder) {
 
@@ -47,4 +53,43 @@ export class HomeComponent implements OnInit {
         }
     }
 
+    onclick(name, index): void {
+     this.isActiveTemplate = index;
+    }
+
+    onClickDownload(): void {
+        // html2canvas(document.querySelector('#tem1')).then(canvas => {
+        //
+        //     /// document.body.appendChild(canvas);
+        //     this.capturedImage = canvas.toDataURL();
+        //     console.log("canvas.toDataURL() -->" + this.capturedImage);
+        //     // this will contain something like (note the ellipses for brevity), console.log cuts it off
+        //     // "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAa0AAAB3CAYAAACwhB/KAAAXr0lEQVR4Xu2dCdiNZf7HP/ZQkpQtaUxDjYYoTSYlURMhGlmKa..."
+        //
+        //
+        //     canvas.toBlob( (blob) =>{
+        //
+        //         //  just pass blob to something expecting a blob
+        //         // somfunc(blob);
+        //
+        //         // Same as canvas.toDataURL(), just longer way to do it.
+        //         var reader = new FileReader();
+        //         reader.readAsDataURL(blob);
+        //         // tslint:disable-next-line:only-arrow-functions
+        //         reader.onloadend =  () => {
+        //             let base64data = reader.result;
+        //             console.log("Base64--> " + base64data);
+        //         }
+        //
+        //     });
+        //
+        //
+        // });
+        html2canvas(this.screen.nativeElement).then(canvas => {
+            this.canvas.nativeElement.src = canvas.toDataURL();
+            this.downloadLink.nativeElement.href = canvas.toDataURL('image/png');
+            this.downloadLink.nativeElement.download = 'marble-diagram.png';
+            this.downloadLink.nativeElement.click();
+        });
+    }
 }
